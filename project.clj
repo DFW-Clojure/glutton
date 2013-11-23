ba60a6187b34b0c6cf8ac4284bfca5753badefae
Alex Robbins <alexander.j.robbins@gmail.com>
2013-11-23 14:26:12 -0600

    Adding twitter api dep.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject glutton "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Data ingestion for the lambda architecture."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-;  :aot [glutton.TopologySubmitter]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [twitter-api "0.7.4"]]
+  ;:aot [glutton.TopologySubmitter]
   ;; include storm dependency only in dev because production storm cluster provides it
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[storm "0.8.1"]
