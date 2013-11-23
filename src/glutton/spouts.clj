cb6ab0f4501766be17d7349a44b7adc1c2d9078b
Chris McFarlen <mcfarlen@yahoo-inc.com>
2013-11-23 14:38:08 -0600

    added mock twitter spout

--- src/glutton/spouts.clj
+++ src/glutton/spouts.clj
@@ -18,3 +18,27 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
         ;; (such as one that reads off of a queue like Kestrel)
         ;; This is an unreliable spout, so it does nothing here
         ))))
+
+(def sample-tweets
+  ["Sencha fresh mint tea on a Yahoo Oktoberfest beer jar with a mince pie. #globalculture pic.twitter.com/twHmqqRvwB"
+   "but 4real tho, who thought fusion earrings would be more stable than the fusion dance!?! Amiright!?"
+   "As argument for why Android doesn't use SysV IPC, they include source for a linux exploit in the NDK docs. :-)"
+   "Just so I don't feel left out #XboxOne pic.twitter.com/xU9zol1tmN"
+   "Wouldn't it be nice to have a list of all the #XboxOne voice commands? We thought so too. http://xbx.lv/1ccfTb6"
+   "Manipulate that vortex and run, you clever girl! #DoctorWho50th #DayoftheDoctor"
+   "#1DDayLive HARRY WOULD SING WRECKING BALL R U KIDDING ME AND LIAM GOING WITH JT"
+   "My limited edition #harrypotter stamps have finally arrived! #itsagoodday http://instagram.com/p/hEhEXNRyT0/"
+   "As requested, I wrote down some of the very basic usages of cravendb in some docs http://robashton.github.io/cravendb/  #clojure"])
+
+
+(defspout mock-twitter-spout ["tweet"]
+  [conf context collector]
+  (spout
+    (nextTuple []
+               (Thread/sleep 1000)
+               (emit-spout! collector [(rand-nth sample-tweets)]))
+    (ack [id]
+         ;; You only need to define this method for reliable spouts
+         ;; (such as one that reads off of a queue like Kestrel)
+         ;; This is an unreliable spout, so it does nothing here
+         )))
--- src/glutton/topology.clj
+++ src/glutton/topology.clj
@@ -5,16 +5,16 @@ More info on the Clojure DSL here:
 
 https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
   (:require [glutton
-             [spouts :refer [type-spout]]
+             [spouts :refer [type-spout mock-twitter-spout]]
              [bolts :refer [stormy-bolt glutton-bolt]]]
             [backtype.storm [clojure :refer [topology spout-spec bolt-spec]] [config :refer :all]])
   (:import [backtype.storm LocalCluster LocalDRPC]))
 
 (defn stormy-topology []
   (topology
-   {"spout" (spout-spec type-spout)}
+   {"spout" (spout-spec mock-twitter-spout)}
 
-   {"stormy-bolt" (bolt-spec {"spout" ["type"]} stormy-bolt :p 2)
+   {"stormy-bolt" (bolt-spec {"spout" ["tweet"]} stormy-bolt :p 2)
     "glutton-bolt" (bolt-spec {"stormy-bolt" :shuffle} glutton-bolt :p 2)}))
 
 (defn run! [& {debug "debug" workers "workers" :or {debug "true" workers "2"}}]
