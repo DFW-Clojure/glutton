4f68ecedad624d60e5c9317a87bc04ccdf0c82c8
Matt Havener <mhavener@yahoo-inc.com>
2013-11-24 10:46:11 -0600

    aggregator bolt

--- src/glutton/bolts.clj
+++ src/glutton/bolts.clj
@@ -17,7 +17,9 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
 
 (defbolt extract-hashtag-bolt ["hashtag"] [{tweet :tweet :as tuple} collector]
   (let [hashtags (map second (re-seq #"\#(\w\w+)" (str tuple)))]
-    (emit-bolt! collector [hashtags] :anchor tuple))
+    (dorun
+      (for [hashtag hashtags]
+        (emit-bolt! collector [hashtag] :anchor tuple))))
   (ack! collector tuple))
 
 (defbolt glutton-bolt ["message"] [{stormy :stormy :as tuple} collector]
@@ -27,17 +29,21 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
 ;; below is untested
 
 (def ^:const WINDOW_SIZE_SEC 10)
+(def ^:const TOP_N 10)
 
 ; the java example does this map<word, counts[]>
 ; this does map<slots, map<words, count>> , which makes sliding the window much simpler
-(defn bump-count! [counts word slot]
+(defn- bump-count! [counts word slot]
   (swap! counts
          (fn [p] (update-in p [slot word] (fnil inc 0)))))
 
-(defn top-n [counts n]
-  (let [word-counts (apply (partial merge-with +) (vals counts)) ; merge all the slots
-        ordered-counts (reverse (sort-by second (vec word-counts)))] ; sort by most popular words
-    (map first (take n ordered-counts)))) ; get the top n most popular
+(defn- top-n [counts n]
+  (let [ordered-counts (reverse (sort-by second (vec counts)))] ; sort by most popular words
+    (into {} (take n ordered-counts)))) ; get the top n most popular
+
+(defn- top-n-slotted [slotted-counts n]
+  (let [word-counts (apply (partial merge-with +) (vals slotted-counts))] ; merge all the slots
+    (top-n word-counts n)))
 
 ; replaced by the builtin tick spout
 ;(defn current-slot []
@@ -47,7 +53,7 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
 ;(def counts (atom {}))
 
 ; from storm-starter/src/jvm/storm/starter/util/TupleHelpers.java
-(defn is-tick [tuple]
+(defn- is-tick [tuple]
   (= (.getSourceStreamId tuple) (Constants/SYSTEM_TICK_STREAM_ID)))
 
 ; prepared bolt see https://github.com/nathanmarz/storm/wiki/Clojure-DSL#prepared-bolts
@@ -65,8 +71,26 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
                  (do ; dosync?
                    (swap! slot (fn [s] (mod (inc s) WINDOW_SIZE_SEC)))
                    (swap! counts dissoc @slot)
-                   (emit-bolt! collector [(top-n @counts 3)] :anchor tuple))
+                   (emit-bolt! collector [(top-n-slotted @counts TOP_N)] :anchor tuple))
                  (let [tag (:hashtag tuple)] ; NOT (tuple :hashtag)
                    (bump-count! counts tag @slot)
 ;                   (emit-bolt! collector [@counts] :anchor tuple)
+                   (ack! collector tuple)))))))
+
+(defbolt count-agg-bolt ["agg-counts"]
+  {:prepare true
+   ; https://groups.google.com/forum/#!topic/storm-user/9M6O2fo0ugM
+   ; sends this bolt a 'tick' tuple every sec
+   :conf {"topology.tick.tuple.freq.secs", 1}}
+  [config context collector]
+  (let [agg-counts (atom {})]
+    (bolt
+      (execute [tuple]
+               (if (is-tick tuple)
+                 (do
+                   (swap! agg-counts top-n TOP_N) ; every second, reduce to top N
+                   (emit-bolt! collector [@agg-counts] :anchor tuple))
+                 (let [counts (:counts tuple)]
+                   ; merge in top N from all bolts (should be no key collision)
+                   (swap! agg-counts merge counts)
                    (ack! collector tuple)))))))
--- src/glutton/topology.clj
+++ src/glutton/topology.clj
@@ -6,7 +6,7 @@ More info on the Clojure DSL here:
 https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
   (:require [glutton
              [spouts :refer [type-spout mock-twitter-spout]]
-             [bolts :refer [stormy-bolt glutton-bolt sliding-count-bolt extract-hashtag-bolt]]
+             [bolts :refer [stormy-bolt glutton-bolt sliding-count-bolt extract-hashtag-bolt count-agg-bolt]]
              [twitter :refer [twitter-spout]]
              ]
             [backtype.storm [clojure :refer [topology spout-spec bolt-spec]] [config :refer :all]])
@@ -21,7 +21,8 @@ https://github.com/nathanmarz/storm/wiki/Clojure-DSL"
     ;"stormy-bolt" (bolt-spec {"spout" ["type"]} stormy-bolt :p 2)
     ;"glutton-bolt" (bolt-spec {"stormy-bolt" :shuffle} glutton-bolt :p 2)
     "hashtag-bolt" (bolt-spec {"spout" ["tweet"]} extract-hashtag-bolt :p 2)
-    "sliding-count-bolt" (bolt-spec {"hashtag-bolt" ["hashtag"]} sliding-count-bolt)
+    "sliding-count-bolt" (bolt-spec {"hashtag-bolt" ["hashtag"]} sliding-count-bolt :p 4)
+    "count-agg-bolt" (bolt-spec {"sliding-count-bolt" :global} count-agg-bolt)
     }))
 
 (defn run! [& {debug "debug" workers "workers" :or {debug "true" workers "2"}}]
